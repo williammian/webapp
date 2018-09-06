6160aba0ebd34f2fc156ded5e2fc8e1790b37bf1
willi <willi@LAPTOP-B6ER7NR3>
2018-09-05 22:47:24 -0300

    webapp

--- script_objetos_banco_mysql.sql
+++ script_objetos_banco_mysql.sql
@@ -1,18 +1,69 @@
-INSERT INTO webapp.tb_grupo(id_grupo,
+/*CRIANDO A TABELA DE GRUPOS*/
+CREATE TABLE tb_grupo
+(
+  
+  id_grupo     INT           auto_increment PRIMARY KEY NOT NULL,
+  ds_nome      VARCHAR(50)   NOT NULL,
+  ds_descricao VARCHAR(200)  NOT NULL
+);
+
+
+/*CRIANDO A TABELA DE PERMISSÕES*/
+CREATE TABLE tb_permissao
+(
+   id_permissao INT auto_increment PRIMARY KEY NOT NULL,
+   ds_permissao VARCHAR(50)    NOT NULL,
+   ds_descricao VARCHAR(200)   NOT NULL   
+);
+
+
+
+/*CRIANDO A TABELA DE USUÁRIOS*/
+CREATE TABLE tb_usuario
+(
+  id_usuario INT auto_increment PRIMARY KEY NOT NULL,
+  ds_nome    VARCHAR(60)      NOT NULL,
+  ds_login   VARCHAR(60)      NOT NULL,
+  ds_senha   VARCHAR(400)     NOT NULL,
+  fl_ativo   smallint        NOT NULL
+);
+
+/*CRIANDO A TABELA DE USUÁRIO X GRUPO*/
+CREATE TABLE tb_usuario_x_grupo
+(
+  id_usuario INT NOT NULL,  
+  id_grupo   INT NOT NULL,
+  CONSTRAINT PK_USU_GRUP   PRIMARY KEY(id_usuario,id_grupo),
+  FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id_usuario), 
+  FOREIGN KEY(id_grupo)  REFERENCES tb_grupo(id_grupo)
+);
+
+/*CRIANDO A TABELA DE PERMISSÃO X GRUPO*/
+CREATE TABLE tb_permissao_x_grupo
+(
+  id_permissao INT NOT NULL,  
+  id_grupo     INT NOT NULL,
+  CONSTRAINT PK_PER_GRUP   PRIMARY KEY(id_permissao,id_grupo),
+  CONSTRAINT FK_PERM_1 FOREIGN KEY(id_permissao) REFERENCES tb_permissao(id_permissao), 
+  CONSTRAINT FK_GRUP_1 FOREIGN KEY(id_grupo)  REFERENCES tb_grupo(id_grupo)
+);
+
+
+INSERT INTO tb_grupo(id_grupo,
                              ds_nome,
                              ds_descricao) 
                       VALUES(1,
                               'ADMINISTRADORES', 
                               'Adminitrador');
 
-INSERT INTO webapp.tb_grupo(id_grupo,
+INSERT INTO tb_grupo(id_grupo,
                              ds_nome,
                              ds_descricao) 
                       VALUES(2,
                               'USUARIOS', 
                               'Usuários Comum');
 
-INSERT INTO webapp.tb_grupo(id_grupo,
+INSERT INTO tb_grupo(id_grupo,
                              ds_nome,
                              ds_descricao) 
                       VALUES(3,
@@ -21,34 +72,34 @@ INSERT INTO webapp.tb_grupo(id_grupo,
 
 
                               
-INSERT INTO webapp.tb_usuario (id_usuario,
+INSERT INTO tb_usuario (id_usuario,
                                 ds_nome,
                                 ds_login,
                                 ds_senha,
                                 fl_ativo)
                         VALUES(1,
-                                'Cícero Ednilson',
-                                'cicero',
+                                'William Mian',
+                                'william',
                                 '$2a$10$YYe9VtFGZoWvrNSZNV/AeuVSTOMQLxcGia4IQEl/yVaxrfAnPDcuO',
                                  1);
                                  
                                  
 
-INSERT INTO webapp.tb_permissao(id_permissao,
+INSERT INTO tb_permissao(id_permissao,
                                   ds_permissao,
                                   ds_descricao)
                            VALUES(1,
                                    'ROLE_CADASTROUSUARIO',
                                    'CADASTRO DE NOVOS USUÁRIOS');
                                    
-INSERT INTO webapp.tb_permissao(id_permissao,
+INSERT INTO tb_permissao(id_permissao,
                                   ds_permissao,
                                   ds_descricao)
                            VALUES(2,
                                    'ROLE_CONSULTAUSUARIO',
                                    'CONSULTA DE USUÁRIOS');                                   
 
-INSERT INTO webapp.tb_permissao(id_permissao,
+INSERT INTO tb_permissao(id_permissao,
                                   ds_permissao,
                                   ds_descricao)
                            VALUES(3,
@@ -56,18 +107,18 @@ INSERT INTO webapp.tb_permissao(id_permissao,
                                    'ADMINISTRAÇÂO COMPLETA');                                   
 
 
-INSERT INTO webapp.tb_usuario_x_grupo(id_usuario,id_grupo)VALUES(1,1);
+INSERT INTO tb_usuario_x_grupo(id_usuario,id_grupo)VALUES(1,1);
 
 
 
 /*ROLE_CADASTROUSUARIO x BACKOFFICE*/
-INSERT INTO webapp.tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(1,3); 
+INSERT INTO tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(1,3); 
 
 /*ROLE_CONSULTAUSUARIO x USUARIOS*/
-INSERT INTO webapp.tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(2,2);
+INSERT INTO tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(2,2);
 
 /*ROLE_ADMIN x ADMINISTRADORES*/
-INSERT INTO webapp.tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(3,1);
+INSERT INTO tb_permissao_x_grupo(id_permissao,id_grupo)VALUES(3,1);
 
 
 
--- src/main/java/br/com/wm/webapp/WebappApplication.java
+++ src/main/java/br/com/wm/webapp/WebappApplication.java
@@ -2,21 +2,26 @@ package br.com.wm.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebappApplication.class);
-    }
+public class WebappApplication {
     
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
-	
-
-	
+		
 }
+
+//@SpringBootApplication
+//public class WebappApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(WebappApplication.class);
+//    }
+//    
+//	public static void main(String[] args) {
+//		SpringApplication.run(WebappApplication.class, args);
+//	}
+//		
+//}
